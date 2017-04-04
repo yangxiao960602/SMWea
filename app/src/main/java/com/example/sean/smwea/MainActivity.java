efefa59a8b89881c2e7584aa6f55d03b42cff8f4
Sean_Yan <heroyagxiao@126.com>
2017-04-04 16:45:29 +0800

    完成遍历省市县列表功能

--- app/src/main/java/com/example/sean/smwea/MainActivity.java
+++ app/src/main/java/com/example/sean/smwea/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sean.smwea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
